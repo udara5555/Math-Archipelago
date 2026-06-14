ac27e3778a89faea417860ad59f126f2f45f7356
udara5555 <udara.chathuranga.md@gmail.com>
2026-06-14 15:44:02 +0530

    arithmetic done

--- Assets/Scripts/ArithmeticUI.cs
+++ Assets/Scripts/ArithmeticUI.cs
@@ -3,14 +3,37 @@ using UnityEngine;
 public class ArithmeticUI : MonoBehaviour
 {
     public GameObject instructionsImage;
+    public GameObject instructionsPanel;
+
+    [Tooltip("Assign all content GameObjects inside InstructionsPanel: Addition, Substraction, Multiplication, Division")]
+    public GameObject[] instructionContents;
 
     void Start()
     {
         instructionsImage.SetActive(false);
+        instructionsPanel.SetActive(false);
     }
 
     public void ToggleInstructions()
     {
         instructionsImage.SetActive(!instructionsImage.activeSelf);
+    }
+
+    public void ShowInstructionPanel(GameObject content)
+    {
+        // Disable all content first
+        foreach (var item in instructionContents)
+        {
+            item.SetActive(false);
+        }
+
+        // Enable the panel and the selected content
+        instructionsPanel.SetActive(true);
+        content.SetActive(true);
+    }
+
+    public void CloseInstructionsPanel()
+    {
+        instructionsPanel.SetActive(false);
     }
 }
